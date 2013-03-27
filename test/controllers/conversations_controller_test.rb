1fffad3ee5fbc963a510a8aacd9c54412ca96ccd
Ivan Efremov <st8998@gmail.com>
2013-03-27 10:07:12 +0400

    conversations controller tests

---
+++ test/controllers/conversations_controller_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class ConversationsControllerTest < ActionController::TestCase
+
+  setup do
+    @user1 = User.create(conversations: [Conversation.create])
+    @user2 = User.create(conversations: [Conversation.create])
+  end
+
+  test 'user sees only his conversations' do
+    @controller.stub(:current_user, @user1) do
+      assert_equal @controller.conversations.to_a, @user1.conversations.to_a
+
+      get(:show, id: @user1.conversations.first)
+      assert response.success?
+    end
+  end
+
+  test 'send 404 when access foreign conversations' do
+    @controller.stub(:current_user, @user1) do
+      assert_raises(ActionController::RoutingError) do
+        get :show, id: @user2.conversations.first
+      end
+    end
+  end
+
+end
