ba517276c98bc7d42974bfd579e3649107f06963
Ivan Efremov <st8998@gmail.com>
2013-04-17 10:51:56 +0400

    fix assets compilation

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -4,7 +4,7 @@ class Conversation < ActiveRecord::Base
   has_many :user_conversations, dependent: :destroy
   has_many :users, through: :user_conversations
 
-  has_many :messages, dependent: :destroy, order: :id
+  has_many :messages, lambda { order :id }, dependent: :destroy
 
   after_save :ensure_slug
 
